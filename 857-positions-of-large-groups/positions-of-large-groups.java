5791ea86451d194b2cff0205bc9c3d45d2960964
Nayan Kamble <nayankamble0906@gmail.com>
2026-09-19 20:06:02 +0530

    Time: 1 ms (99.88%) | Memory: 45.9 MB (39.95%) - LeetSync

---
+++ 857-positions-of-large-groups/positions-of-large-groups.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<List<Integer>> largeGroupPositions(String s) {
+        List<List<Integer>> result = new ArrayList<>();
+        int start = 0;
+
+        for (int i = 1; i <= s.length(); i++) {
+            if (i == s.length() || s.charAt(i) != s.charAt(start)) {
+
+                if (i - start >= 3) {
+                    result.add(Arrays.asList(start, i - 1));
+                }
+
+                start = i;
+            }
+        }
+        return result;
+    }
+}
